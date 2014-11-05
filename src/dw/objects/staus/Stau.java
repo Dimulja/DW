a7671ab58c607a83696de41b9ae915d8f08f5ec3
Dmytro Shlyakhov <Dmytro.Shlyakhov@uni-duesseldorf.de>
2014-11-05 15:12:27 +0100

    YES(nur rumgespielt)

--- src/dw/objects/staus/Stau.java
+++ src/dw/objects/staus/Stau.java
@@ -33,8 +33,8 @@ public class Stau {
 	}
 
 	
-	public void setEigenschaft(String Eigenschaft, String Wert){
-		this.Eigenschaft=Eigenschaft;
-		
-	}
+//	public void setEigenschaft(String Eigenschaft, String Wert){
+//		this.Eigenschaft=Eigenschaft;
+//		
+//	}
 }
